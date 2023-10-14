4d8b1f01b4006d25af50f4484e95bdc342a850c8
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-10-14 11:31:31 +0900

    ping status

--- src/App.tsx
+++ src/App.tsx
@@ -42,6 +42,7 @@ export default function App() {
 
   useEffect(() => {
     const i = setInterval(() => {
+      client.current?.pingStatus;
       client.current?.sync();
     }, 100);
     return () => clearInterval(i);
