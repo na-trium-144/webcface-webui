cbb87fdfe3df21ed3467c22a6b1dcf0a96f95ced
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-12-19 18:51:56 +0900

    connectionInfoCardのoverflow修正

--- src/components/connectionInfoCard.tsx
+++ src/components/connectionInfoCard.tsx
@@ -30,57 +30,59 @@ export function ConnectionInfoCard(props: Props) {
   return (
     <Card title={`Connection Info`}>
       <div className="w-full h-full overflow-auto">
-        <p className="text-sm flex items-center">
-          <span className="">Server:</span>
-          <span className="pl-1">{props.client?.serverName}</span>
-          <span className="pl-1">{props.client?.serverVersion}</span>
-          <span className="pl-2">
-            <WifiStrength level={props.client?.connected ? 3 : 0} />
-          </span>
-        </p>
-        <p className="text-sm">
-          <span className="">WebUI:</span>
-          <span className="pl-1">{webuiVersion}</span>
-          <span className="text-xs pl-2">(</span>
-          <span className="text-xs">Client:</span>
-          <span className="text-xs pl-1">{webcfaceVersion}</span>
-          <span className="text-xs">)</span>
-        </p>
-        <p className="text-sm">
-          <span className="">Members:</span>
-        </p>
-        <ul>
-          {props.client?.members().map((m, i) => (
-            <li key={i} className="pl-4 text-sm flex items-center">
-              <span className="">
-                {m.name}
-                {m.remoteAddr === "127.0.0.1" || (
-                  <span className="text-xs pl-0.5">({m.remoteAddr})</span>
-                )}
-              </span>
-              <div className="pl-1">
-                <WifiStrength
-                  level={
-                    m.pingStatus === null
-                      ? 0
-                      : m.pingStatus < 10
-                      ? 3
-                      : m.pingStatus < 100
-                      ? 2
-                      : 1
-                  }
-                />
-              </div>
-              <span className="pl-1">
-                {m.pingStatus !== null ? m.pingStatus : "?"}
-              </span>
-              <span className="pl-1">ms</span>
-              <span className="pl-0.5">:</span>
-              <span className="pl-1">{m.libName}</span>
-              <span className="pl-1">{m.libVersion}</span>
-            </li>
-          ))}
-        </ul>
+        <div className="w-max">
+          <p className="text-sm flex items-center">
+            <span className="">Server:</span>
+            <span className="pl-1">{props.client?.serverName}</span>
+            <span className="pl-1">{props.client?.serverVersion}</span>
+            <span className="pl-2">
+              <WifiStrength level={props.client?.connected ? 3 : 0} />
+            </span>
+          </p>
+          <p className="text-sm">
+            <span className="">WebUI:</span>
+            <span className="pl-1">{webuiVersion}</span>
+            <span className="text-xs pl-2">(</span>
+            <span className="text-xs">Client:</span>
+            <span className="text-xs pl-1">{webcfaceVersion}</span>
+            <span className="text-xs">)</span>
+          </p>
+          <p className="text-sm">
+            <span className="">Members:</span>
+          </p>
+          <ul>
+            {props.client?.members().map((m, i) => (
+              <li key={i} className="pl-4 text-sm flex items-center">
+                <span className="">
+                  {m.name}
+                  {m.remoteAddr === "127.0.0.1" || (
+                    <span className="text-xs pl-0.5">({m.remoteAddr})</span>
+                  )}
+                </span>
+                <div className="pl-1">
+                  <WifiStrength
+                    level={
+                      m.pingStatus === null
+                        ? 0
+                        : m.pingStatus < 10
+                        ? 3
+                        : m.pingStatus < 100
+                        ? 2
+                        : 1
+                    }
+                  />
+                </div>
+                <span className="pl-1">
+                  {m.pingStatus !== null ? m.pingStatus : "?"}
+                </span>
+                <span className="pl-1">ms</span>
+                <span className="pl-0.5">:</span>
+                <span className="pl-1">{m.libName}</span>
+                <span className="pl-1">{m.libVersion}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </Card>
   );
