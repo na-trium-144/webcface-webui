128007c3a8223965817939da5181cdaf4ac111bc
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-01-17 18:33:22 +0900

    buildエラー修正

--- src/components/imageCard.tsx
+++ src/components/imageCard.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 export function ImageCard(props: Props) {
   const hasUpdate = useRef<boolean>(true);
-  const imgRef = useRef(null);
+  const imgRef = useRef<HTMLDivElement>(null);
   const prevImgWidth = useRef<number>(0);
   const prevImgHeight = useRef<number>(0);
   const update = useForceUpdate();
@@ -19,30 +19,30 @@ export function ImageCard(props: Props) {
         hasUpdate.current = false;
       }
       const ratio = props.image.get().width / props.image.get().height;
-      if (!isNaN(ratio)) {
-        if (
-          prevImgWidth.current !== imgRef.current.clientWidth ||
-          prevImgHeight.current !== imgRef.current.clientHeight
-        ) {
-          prevImgWidth.current = imgRef.current.clientWidth;
-          prevImgHeight.current = imgRef.current.clientHeight;
-          const imgRatio = prevImgWidth.current / prevImgHeight.current;
-          let newWidth: number | undefined = undefined,
-            newHeight: number | undefined = undefined;
-          if (ratio > imgRatio) {
-            newWidth = prevImgWidth.current;
-          } else {
-            newHeight = prevImgHeight.current;
-          }
-          console.log(`re-request ${newWidth} x ${newHeight}`)
-          props.image.request({
-            width: newWidth,
-            height: newHeight,
-            compressMode: imageCompressMode.jpeg,
-            quality: 50,
-            frameRate: 10,
-          });
+      if (
+        !isNaN(ratio) &&
+        imgRef.current !== null &&
+        (prevImgWidth.current !== imgRef.current.clientWidth ||
+          prevImgHeight.current !== imgRef.current.clientHeight)
+      ) {
+        prevImgWidth.current = imgRef.current.clientWidth;
+        prevImgHeight.current = imgRef.current.clientHeight;
+        const imgRatio = prevImgWidth.current / prevImgHeight.current;
+        let newWidth: number | undefined = undefined,
+          newHeight: number | undefined = undefined;
+        if (ratio > imgRatio) {
+          newWidth = prevImgWidth.current;
+        } else {
+          newHeight = prevImgHeight.current;
         }
+        console.log(`re-request ${newWidth} x ${newHeight}`);
+        props.image.request({
+          width: newWidth,
+          height: newHeight,
+          compressMode: imageCompressMode.jpeg,
+          quality: 50,
+          frameRate: 10,
+        });
       }
     }, 50);
     return () => clearInterval(i);
