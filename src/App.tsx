cca41bbdaecd8cf3b3b43ea6e33423435bb61a9b
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-03-08 18:38:31 +0900

    useRef削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { Client, LogLine } from "webcface";
 import "./index.css";
 import { LayoutMain } from "./components/layout";
