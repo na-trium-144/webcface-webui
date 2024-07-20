ea6ee7dcdb514fea297d3edfee98463793e646e7
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-07-20 21:43:19 +0900

    グラフにグリッドを追加

--- src/components/valueCard.tsx
+++ src/components/valueCard.tsx
@@ -3,7 +3,7 @@ import { Value } from "webcface";
 import { useState, useEffect, useRef } from "react";
 import { WebglPlot, WebglLine, ColorRGBA } from "webgl-plot";
 import ReactSlider from "react-slider";
-import { format, addMilliseconds } from "date-fns";
+import { format, addMilliseconds, setMilliseconds } from "date-fns";
 
 interface Props {
   value: Value;
@@ -135,16 +135,16 @@ export function ValueCard(props: Props) {
   }, []);
 
   const followValue = (f: boolean) => {
-    if(f){
+    if (f) {
       setDisplayPos(maxPos);
       currentPos.current = maxPos;
       isLatest.current = true;
-    }else{
-      if(maxPos === currentPos.current && maxPos > 0){
+    } else {
+      if (maxPos === currentPos.current && maxPos > 0) {
         currentPos.current = maxPos - 1;
         setDisplayPos(maxPos - 1);
       }
-      if(currentPos.current < maxPos){
+      if (currentPos.current < maxPos) {
         isLatest.current = false;
       }
     }
@@ -171,13 +171,66 @@ export function ValueCard(props: Props) {
     }
   }, [cursorX, displayPos, displayMinY, displayMaxY]);
 
+  let yTick = Math.pow(10, Math.floor(Math.log10(Math.max(1, displayMaxY - displayMinY))));
+  if ((displayMaxY - displayMinY) / yTick <= 2) {
+    yTick /= 5;
+  } else if ((displayMaxY - displayMinY) / yTick <= 5) {
+    yTick /= 2;
+  }
+  const displayMaxX = addMilliseconds(
+    startTime,
+    maxPos === 0 ? data.current.length : displayPos + numPoints
+  );
   return (
     <Card title={`${props.value.member.name}:${props.value.name}`}>
       <div className="flex flex-col h-full">
         <div className="flex-1 w-full min-h-0 flex flex-row text-xs">
-          <div className="flex-1 h-full min-w-0 pt-2 pb-6 relative">
+          <div className="flex-1 h-full min-w-0 pt-2 relative">
+            {/*
             <span className="absolute top-2 left-0">{displayMaxY}</span>
-            <span className="absolute bottom-6 left-0">{displayMinY}</span>
+            <span className="absolute left-0">{displayMinY}</span>
+            */}
+            {[
+              ...new Array(
+                Math.max(
+                  Math.floor(displayMaxY / yTick) -
+                    Math.ceil(displayMinY / yTick) +
+                    1,
+                  0
+                )
+              ).keys(),
+            ]
+              .map((_, i) => (Math.ceil(displayMinY / yTick) + i) * yTick)
+              .map((y, i) => (
+                <div
+                  key={i}
+                  className="absolute w-full h-auto left-0 border-b border-gray-300 text-gray-500"
+                  style={{
+                    bottom:
+                      ((y - displayMinY) / (displayMaxY - displayMinY)) *
+                      (canvasMain.current?.height || 0),
+                  }}
+                >
+                  {y}
+                </div>
+              ))}
+            {[...new Array(4).keys()]
+              .map(
+                (_, i) => setMilliseconds(displayMaxX, 0).getTime() - i * 1000
+              )
+              .map((x, i) => (
+                <div
+                  key={i}
+                  className="absolute w-auto h-full bottom-0 border-r border-gray-300 text-gray-500"
+                  style={{
+                    right:
+                      (displayMaxX.getTime() - x) / numPoints *
+                      (canvasMain.current?.width || 0),
+                  }}
+                >
+                  <span className="absolute bottom-0 right-0">{format(x, ":ss")}</span>
+                </div>
+              ))}
             <div className="w-full h-full relative" ref={canvasDiv}>
               <canvas
                 className=""
@@ -197,33 +250,19 @@ export function ValueCard(props: Props) {
                 time={
                   cursorX != null
                     ? addMilliseconds(
-                        startTime,
-                        (maxPos === 0
-                          ? data.current.length
-                          : displayPos + numPoints) -
-                          numPoints +
-                          cursorX
+                        displayMaxX,
+                        -numPoints +
+                          (cursorX / (canvasMain.current?.width || 1)) *
+                            numPoints
                       )
                     : null
                 }
               />
             </div>
-            <span className="absolute left-0 bottom-1">
-              {format(addMilliseconds(startTime, displayPos), "H:mm:ss")}
-            </span>
-            <span className="absolute right-0 bottom-1">
-              {format(
-                addMilliseconds(
-                  startTime,
-                  maxPos === 0 ? data.current.length : displayPos + numPoints
-                ),
-                "H:mm:ss"
-              )}
-            </span>
           </div>
         </div>
-        <div className="flex-none flex items-center space-x-1">
-          <span className="text-sm">Time:</span>
+        <div className="flex-none flex items-center space-x-1 text-xs">
+          <span>Time:</span>
           <ReactSlider
             className="w-full h-4"
             renderTrack={SliderTrack}
@@ -241,6 +280,15 @@ export function ValueCard(props: Props) {
               }
             }}
           />
+          <span>
+            {format(
+              addMilliseconds(
+                startTime,
+                maxPos === 0 ? data.current.length : maxPos + numPoints
+              ),
+              "H:mm:ss"
+            )}
+          </span>
         </div>
         <div className="flex-none flex items-center px-2 space-x-1 text-sm">
           <input
@@ -249,7 +297,11 @@ export function ValueCard(props: Props) {
             checked={isLatest.current}
             onChange={(e) => followValue(e.target.checked)}
           />
-          <label htmlFor={`follow-${props.value.member.name}:${props.value.name}-value`}>Follow Latest Data</label>
+          <label
+            htmlFor={`follow-${props.value.member.name}:${props.value.name}-value`}
+          >
+            Follow Latest Data
+          </label>
         </div>
       </div>
     </Card>
