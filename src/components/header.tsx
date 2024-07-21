3f1ec54a022d16e88219d969dc0f4918cbb0444e
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-07-21 20:27:50 +0900

    hostname

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,18 +2,39 @@ import { useEffect } from "react";
 // import version from "../libs/version";
 import { HamburgerButton, CloseSmall } from "@icon-park/react";
 import "../../renderer.d.ts";
+import { Client } from "webcface";
 
 interface Props {
   menuOpen: boolean;
   setMenuOpen: (menuOpen: boolean) => void;
+  client: Client | null;
+  clientHost: string;
+  clientPort: number | null;
 }
 export function Header(props: Props) {
   const title = window.electronAPI ? "WebCFace Desktop" : "WebCFace";
   useEffect(() => {
-    document.title = window.electronAPI
-      ? "WebCFace Desktop"
-      : "WebCFace WebUI";
-  }, []);
+    let i: ReturnType<typeof setTimeout> | null = null;
+    const updateTitle = () => {
+      const title = window.electronAPI ? "WebCFace Desktop" : "WebCFace WebUI";
+      if (props.client?.serverHostName && props.clientPort) {
+        document.title = `${props.client?.serverHostName} (${props.clientHost}:${props.clientPort}) - ${title}`;
+      } else if (props.clientPort) {
+        document.title = `${props.clientHost}:${props.clientPort} - ${title}`;
+      } else {
+        document.title = `${props.clientHost} - ${title}`;
+      }
+      if (!props.client?.serverHostName) {
+        i = setTimeout(updateTitle, 100);
+      }
+    };
+    updateTitle();
+    return () => {
+      if (i !== null) {
+        clearTimeout(i);
+      }
+    };
+  }, [props.client, props.clientHost, props.clientPort]);
   return (
     <div className="flex h-full items-center space-x-2">
       <img className="h-5/6" src="icon.svg" />
