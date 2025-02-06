bceba455f16028912efa58c7c2c509237f03a41e
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-02-06 20:14:41 +0900

    リアルタイムの値表示を追加

--- src/components/gamepadCard.tsx
+++ src/components/gamepadCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { GamepadState } from "../libs/gamepad";
 import { Card } from "./card";
 import { Input } from "./input";
@@ -7,6 +8,25 @@ interface Props {
   gamepadState: GamepadState;
 }
 export function GamepadCard(props: Props) {
+  const [buttonState, setButtonState] = useState<number[]>([]);
+  const [axisState, setAxisState] = useState<number[]>([]);
+  useEffect(() => {
+    const i = setInterval(() => {
+      const gamepad = navigator.getGamepads()[props.gamepadState.index];
+      if (gamepad) {
+        setButtonState(
+          gamepad.buttons.map((b) => Number(b.value || b.pressed || b.touched))
+        );
+        setAxisState(gamepad.axes.slice());
+      }
+    }, 50);
+    return () => clearInterval(i);
+  }, [props.gamepadState]);
+
+  const bgColor = (a: number) =>
+    a > 0
+      ? /* bg-breen-300 */ `rgb(134 239 172 / ${a})`
+      : /* bg-orange-300 */ `rgb(253 186 116 / ${-a})`;
   return (
     <Card title={props.gamepadState.id}>
       <div className="w-full h-full overflow-auto ">
@@ -32,6 +52,47 @@ export function GamepadCard(props: Props) {
               )
             }
           />
+        </div>
+        <div>
+          <span className="text-sm mr-1">Name:</span>
+          <span className="text-sm text-nowrap">{props.gamepadState.id}</span>
+        </div>
+        <div className="text-nowrap">
+          <span className="text-sm mr-1">Buttons:</span>
+          {buttonState.map((b, i) => (
+            <div
+              key={i}
+              className={
+                "text-center text-xs inline-block min-w-7 py-0.5 " +
+                "border-gray-300 border-t border-l border-b " +
+                (i == 0 ? "rounded-l " : "") +
+                (i == buttonState.length - 1 ? "rounded-r border-r " : "")
+              }
+              style={{ background: bgColor(b) }}
+            >
+              <div className="text-xs text-gray-500 ">[{i}]</div>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-row items-center mt-1">
+          <span className="text-sm mr-1">Axes:</span>
+          {axisState.map((a, i) => (
+            <div
+              key={i}
+              className={
+                "text-center inline-block w-10 py-0.5 " +
+                "border-gray-300 border-t border-l border-b " +
+                (i == 0 ? "rounded-l " : "") +
+                (i == axisState.length - 1 ? "rounded-r border-r " : "")
+              }
+              style={{
+                background: bgColor(a),
+              }}
+            >
+              <div className="text-xs text-gray-500 ">[{i}]</div>
+              <div className="text-sm ">{a.toFixed(2)}</div>
+            </div>
+          ))}
         </div>
       </div>
     </Card>
